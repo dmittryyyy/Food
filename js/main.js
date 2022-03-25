ddc7144787807621139907b7d359173c9a7d3ebf
dmittryyyy <hhh963123741@gmail.com>
2022-03-25 16:45:22 +0700

    adding slider, update main.js

--- js/main.js
+++ js/main.js
@@ -286,3 +286,79 @@ window.addEventListener("DOMContentLoaded", () => {
         .then(data => data.json())
         .then(res => console.log(res));
 });
+
+//Slider
+const slides = document.querySelectorAll('.offer__slide'),
+        prevSlide = document.querySelector('.offer__slider-prev'),
+        nextSlide = document.querySelector('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        slidesField = document.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(slidesWrapper).width;
+
+    let slideIndex = 1;
+    let offset = 0;
+
+     if (slides.length < 10) {
+         total.textContent = `0${slides.length}`;
+         current.textContent = `0${slideIndex}`;
+    } else {
+         total.textContent = slides.length;
+         current.textContent = slideIndex;
+    }
+
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
+
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+
+    nextSlide.addEventListener('click', () => {
+        if (offset == +width.slice(0, width.length -2) * (slides.length - 1)) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length -2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex === slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    });
+    
+    prevSlide.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = +width.slice(0, width.length -2) * (slides.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length -2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex === 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    });
